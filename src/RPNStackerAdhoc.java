0ad20af7e58a88999685a1b56983dce74d2e2431
johnny herbert <jhmn@cin.ufpe.br>
2021-11-30 21:50:43 -0300

    [Task 02 - RPNStacker Manual/Simple Scanning]

--- src/RPNStackerAdhoc.java
+++ src/RPNStackerAdhoc.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.LinkedList;
 
 public class RPNStackerAdhoc {
 
@@ -11,49 +12,95 @@ public class RPNStackerAdhoc {
 		double num1 = 0, num2 = 0, result = 0;
 		String caracter = "-";
 		File file = new File("files/Calc1.stk");
-        Scanner in = new Scanner(file);
-        
-        while (in.hasNext()) {
-            caracter = in.nextLine();
+		LinkedList<Token> tokens = null;
+		try {
+			tokens = scanning(file);
+			for (int i = 0; i < tokens.size(); i++) {
+				Token token = tokens.get(i);
+				switch (token.lexeme) {
+				case "+": {
+					num1 = (double)pilha.pop();
+					num2 = (double)pilha.pop();
+					result = num1 + num2;
+					pilha.push(result);
+					break;
+				}
+				case "-": {
+					num1 = (double)pilha.pop();
+					num2 = (double)pilha.pop();
+					result = num2 - num1;
+					pilha.push(result);
+					break;
+				}
+				case "*": {
+					num1 = (double)pilha.pop();
+					num2 = (double)pilha.pop();
+					result = num1 * num2;
+					pilha.push(result);
+					break;
+				}
+				case "/": {
+					num1 = (double)pilha.pop();
+					num2 = (double)pilha.pop();
+					result = num2 / num1;
+					pilha.push(result);
+					break;
+				}
+				default:
+					pilha.push(Double.parseDouble(token.lexeme));
+					break;
+				}
+			}
 
+			System.out.println(pilha.pop());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+	public static LinkedList<Token> scanning(File file) throws Exception {
+		String caracter = "-";
+		Scanner in = new Scanner(file);
+		LinkedList<Token> tokens = new LinkedList<Token>();
+		boolean erro = false;
+		while (in.hasNext() && !erro) {
+			TokenType tokenType = null;
+			caracter = in.nextLine();
 			switch (caracter) {
 			case "+": {
-				num1 = (double)pilha.pop();
-				num2 = (double)pilha.pop();
-				result = num1 + num2;
-				pilha.push(result);
+				tokenType = TokenType.PLUS;
 				break;
 			}
 			case "-": {
-				num1 = (double)pilha.pop();
-				num2 = (double)pilha.pop();
-				result = num2 - num1;
-				pilha.push(result);
+				tokenType = TokenType.MINUS;
 				break;
 			}
 			case "*": {
-				num1 = (double)pilha.pop();
-				num2 = (double)pilha.pop();
-				result = num1 * num2;
-				pilha.push(result);
+				tokenType = TokenType.STAR;
 				break;
 			}
 			case "/": {
-				num1 = (double)pilha.pop();
-				num2 = (double)pilha.pop();
-				result = num2 / num1;
-				pilha.push(result);
+				tokenType = TokenType.SLASH;
 				break;
 			}
 			default:
-				pilha.push(Double.parseDouble(caracter));
+				//verificando se é número
+				if(caracter.matches("[+-]?\\d*(\\.\\d+)?")) {
+					tokenType = TokenType.NUM;
+				}
+				else {
+					throw new Exception("Error: Unexpected character: " + caracter);
+				}
 				break;
 			}
+			Token token = new Token(tokenType, caracter);
+			tokens.add(token);
 		}
-	
-        in.close();
-        
-		System.out.println(pilha.pop());
+
+		in.close();
+		return tokens;
+
 	}
 
 }
---
+++ src/TokenType.java
@@ -0,0 +1,8 @@
+
+public enum TokenType {
+	// Literals.
+		NUM,
+
+		// Single-character tokens for operations.
+		MINUS, PLUS, SLASH, STAR
+}	
